6992fb0154e2a994ef668d6b973e7b4db39e949d
sami <samif3443@gmail.com>
2023-03-06 13:05:04 -0800

    Add hamburgers.js file

---
+++ hamburgers.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger');
+const navnavMenu = document.querySelector('.navnav-menu')
+
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navnavMenu.classList.toggle('active')
+})
+
+document.querySelectorAll('navnav-item').forEach(n => 
+ n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navnavMenu.classList.remove('active');
+ }))
+
+
+
