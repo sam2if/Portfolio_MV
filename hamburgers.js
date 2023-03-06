f731061f39512dfbd3ad46cdadc27aadec45198f
samuel <samif1@gmail.com>
2023-03-07 01:07:10 +0300

    fixing linter errors

--- hamburgers.js
+++ hamburgers.js
@@ -1,17 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
-const navnavMenu = document.querySelector('.navnav-menu')
-
+const navnavMenu = document.querySelector('.navnav-menu');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  navnavMenu.classList.toggle('active')
-})
+  navnavMenu.classList.toggle('active');
+});
 
-document.querySelectorAll('navnav-item').forEach(n => 
- n.addEventListener('click', () => {
+document.querySelectorAll('navnav-item').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navnavMenu.classList.remove('active');
- }))
-
-
-
+}));
