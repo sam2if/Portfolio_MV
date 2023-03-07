e931b8e4811a35789111c259a5db4076f454a336
sami <samif3443@gmail.com>
2023-03-07 04:43:31 -0800

    Update hamburger.js

--- hamburgers.js
+++ hamburgers.js
@@ -1,12 +1,22 @@
 const hamburger = document.querySelector('.hamburger');
 const navnavMenu = document.querySelector('.navnav-menu');
+const contact = document.querySelector('.navnav-item')
+document.querySelectorAll('a[href^="#"').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    navnavMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    document.querySelector(this.getAttribute("href").scrollIntoView({
+      behavior: "smooth"
+    }))
+  })
+})
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navnavMenu.classList.toggle('active');
+  
 });
 
-document.querySelectorAll('navnav-item').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
+document.querySelectorAll('navnav-item').forEach(n => n.addEventListener('click', () => {
   navnavMenu.classList.remove('active');
 }));
