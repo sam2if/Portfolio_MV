d81f79ac00c54a4a564e558696e7e051de1d500e
kayemba <kayembejonathan5@gmail.com>
2023-03-08 15:02:47 +0200

    add data

--- datastorage.js
+++ datastorage.js
@@ -1,1 +1,21 @@
 const a = document.getElementById('#form');
+const nameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const textAreaBoxInput = document.getElementById('msg');
+const formStorage = document.getElementById('form');
+
+let userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+if (userInfo !== null) {
+  if (userInfo.hasOwnProperty.call('username')) {
+    nameInput.value = userInfo.username;
+  }
+  if (userInfo.hasOwnProperty.call('email')) {
+    emailInput.value = userInfo.email;
+  }
+  if (userInfo.hasOwnProperty.call('msg')) {
+    textAreaBoxInput.value = userInfo.msg;
+  }
+} else {
+  userInfo = {};
+}
