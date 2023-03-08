5cbc0914e9a2bb9dbb15359d107813231fe35466
kayemba <kayembejonathan5@gmail.com>
2023-03-08 14:57:04 +0200

    add form validation

--- form_validation.js
+++ form_validation.js
@@ -1,1 +1,25 @@
 console.log("myname");
+
+const form = document.querySelector('.form');
+const submit = document.getElementById('get-in-touch-button');
+
+function checkUppercase(e) {
+  const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+  const email = document.getElementById('email').value;
+  const erroMessagediv = document.querySelector('#erroMessagediv');
+
+  if (!regex.test(email)) {
+    e.preventDefault();
+
+    erroMessagediv.innerHTML = 'Check your email  address';
+    return;
+  }
+
+  if (erroMessagediv.querySelector('p') !== null) {
+    const paragraph = erroMessagediv.querySelector('p');
+    erroMessagediv.removeChild(paragraph);
+    erroMessagediv.getElementsByClassName.display = 'none';
+    submit.style.marginTop = '40px';
+  }
+}
